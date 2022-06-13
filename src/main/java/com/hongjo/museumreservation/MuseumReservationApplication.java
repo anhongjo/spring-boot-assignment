8c65ab0e6b02785a458a065d089d938691ee2c3c
cocoblue <cocoblue@kakao.com>
2022-06-13 18:22:11 +0900

    Initial Commit

---
+++ src/main/java/com/hongjo/museumreservation/MuseumReservationApplication.java
@@ -0,0 +1,13 @@
+package com.hongjo.museumreservation;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MuseumReservationApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MuseumReservationApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/hongjo/museumreservation/MuseumReservationApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hongjo.museumreservation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MuseumReservationApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
