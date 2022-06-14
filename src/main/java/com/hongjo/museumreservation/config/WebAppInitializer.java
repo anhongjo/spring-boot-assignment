732a9c69ff58ad6a25e29ef8e5a3539342673de1
cocoblue <cocoblue@kakao.com>
2022-06-14 16:34:18 +0900

    ADD Configuration and first controller

---
+++ src/main/java/com/hongjo/museumreservation/config/WebAppInitializer.java
@@ -0,0 +1,31 @@
+package com.hongjo.museumreservation.config;
+
+import org.springframework.web.filter.CharacterEncodingFilter;
+import org.springframework.web.servlet.support.AbstractAnnotationConfigDispatcherServletInitializer;
+
+import javax.servlet.Filter;
+
+public class WebAppInitializer extends AbstractAnnotationConfigDispatcherServletInitializer {
+    @Override
+    protected Class<?>[] getRootConfigClasses() {
+        return new Class<?>[]{};
+    }
+
+    @Override
+    protected Class<?>[] getServletConfigClasses() {
+        return new Class<?>[]{MvcConfig.class};
+    }
+
+    @Override
+    protected String[] getServletMappings() {
+        return new String[]{"/"};
+    }
+
+    @Override
+    protected Filter[] getServletFilters() {
+        CharacterEncodingFilter encodingFilter = new CharacterEncodingFilter();
+        encodingFilter.setEncoding("UTF-8");
+
+        return new Filter[]{encodingFilter};
+    }
+}
